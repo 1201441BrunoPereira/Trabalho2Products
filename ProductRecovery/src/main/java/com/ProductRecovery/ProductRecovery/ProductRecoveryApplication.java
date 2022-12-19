f1f6cdcfc9ebf77c213ca47a4f14fa6956b08543
1201441BrunoPereira <1201441@isep.ipp.pt>
2022-12-19 15:51:44 +0000

    First commit for recovery products

---
+++ ProductRecovery/src/main/java/com/ProductRecovery/ProductRecovery/ProductRecoveryApplication.java
@@ -0,0 +1,13 @@
+package com.ProductRecovery.ProductRecovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductRecoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductRecoveryApplication.class, args);
+	}
+
+}
---
+++ ProductRecovery/src/test/java/com/ProductRecovery/ProductRecovery/ProductRecoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProductRecovery.ProductRecovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductRecoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
