96eded5e8877531b7501b6b269378d4042a5b273
Giovanna Fantacini <giovanna.fantacini@aluno.ifsp.edu.br>
2022-12-19 21:29:58 +0000

    Product2_C

---
+++ Product2_C/src/main/java/com/Product2_C/Product2_C/Interfaces/RabbitMQ/RabbitMQPublisher.java
@@ -0,0 +1,27 @@
+package com.Product2_C.Product2_C.Interfaces.RabbitMQ;
+
+
+import com.Product2_C.Product2_C.model.Product;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitMQPublisher {
+
+    @Autowired
+    private RabbitTemplate template;
+
+    @Autowired
+    private FanoutExchange fanout;
+
+    public void sendJsonMessage(Product product) throws JsonProcessingException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        String json = ow.writeValueAsString(product);
+        template.convertAndSend(fanout.getName(), "", json);
+    }
+}
