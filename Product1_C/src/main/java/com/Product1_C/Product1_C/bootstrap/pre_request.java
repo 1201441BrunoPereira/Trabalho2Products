1c5356e1c35a48fdf33385abe772f5233e6fb753
1201441BrunoPereira <1201441@isep.ipp.pt>
2022-12-20 13:17:53 +0000

    End products bootstrap

--- Product1_C/src/main/java/com/Product1_C/Product1_C/bootstrap/pre_request.java
+++ Product1_C/src/main/java/com/Product1_C/Product1_C/bootstrap/pre_request.java
@@ -1,5 +1,10 @@
 package com.Product1_C.Product1_C.bootstrap;
 
+import com.Product1_C.Product1_C.Interfaces.repository.ProductRepository;
+import com.Product1_C.Product1_C.model.Product;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +12,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import java.util.List;
 
 @Component
 @Profile("bootstrap")
@@ -18,13 +24,21 @@ public class pre_request implements CommandLineRunner {
     @Autowired
     private DirectExchange exchange;
 
+    @Autowired
+    private ProductRepository productRepository;
+
     int start = 0;
 
     @Override
     @Scheduled(fixedDelay = 1000, initialDelay = 500)
-    public void run(String... args){
+    public void run(String... args) throws JsonProcessingException {
         System.out.println(" [x] Requesting products from recovery system(" + start + ")");
         String response = (String) template.convertSendAndReceive(exchange.getName(), "rpc", start++);
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<Product> pt = objectMapper.readValue(response, new TypeReference<>() {});
+        for (int i=0; i <= pt.size()-1;i++){
+            productRepository.save(pt.get(i));
+        }
         System.out.println(" [.] Got '" + response + "'");
     }
 }
